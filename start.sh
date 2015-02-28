14cf2b67ee4fba38da551107d3848a8057e2f176
Paulo McNally <paulomcnally@gmail.com>
2015-02-28 14:33:30 -0600

    added sudo to command

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # stop containers
-docker stop nginx
+sudo docker stop nginx
 
 # delete containers
-docker ps -a | grep 'nginx' | awk '{print $1}' | xargs docker rm
+sudo docker ps -a | grep 'nginx' | awk '{print $1}' | xargs docker rm
 
 # start container
-docker run --name nginx -d -p 80:80 controlgimnasio/nginx
+sudo docker run --name nginx -d -p 80:80 controlgimnasio/nginx
