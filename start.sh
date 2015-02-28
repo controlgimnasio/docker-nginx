34008f066290107041c60f265091107c6d2056e5
Paulo McNally <paulomcnally@gmail.com>
2015-02-28 15:01:44 -0600

    fix remove containers

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 sudo docker stop nginx
 
 # delete containers
-sudo docker ps -a | grep 'nginx' | awk '{print $1}' | xargs docker rm -f
+sudo docker rm -f $(sudo docker ps -a | grep 'nginx' | awk '{print $1}')
 
 # start container
 sudo docker run --name nginx -d -p 80:80 controlgimnasio/nginx
